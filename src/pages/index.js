e2622b0394d412604e52036fd966a4234286e205
emreMilon <kurt@milon.com>
2021-11-24 14:23:15 +0100

    init: project initialized

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div
+    className="position-relative"
+    style={{ minHeight: "calc(100vh - 70px)" }}
+  >
+    <h2
+      className="position-absolute text-secondary"
+      style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
+    >
+     Welcome To CRM FORECAST
+    </h2>
+    </div>
+  )
+}
+
+export default Home
