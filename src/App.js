2bbb9988208cbb2f3143e0a01affbd27ebcd7130
emreMilon <kurt@milon.com>
2021-11-24 16:05:32 +0100

    add: redux template, navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import  PageRender  from './PageRender'
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
       <Route exact path="/" component={PageRender} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:slug" component={PageRender} />
       </Switch>
+      <Footer />
     </Router>
   )
 }
